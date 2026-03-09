54488bebb6af8eed05b74cbc445cfc48fbba53c2
youneschaghica-hub <youneschaghica@gmail.com>
2026-03-08 23:32:41 -0400

    les méthodes de la classe Voiture

--- atelier 1.py
+++ atelier 1.py
@@ -3,3 +3,8 @@ class Voiture:
         self.matricule = matricule
         self.marque = marque
         self.couleur = couleur
+
+    def afficherInformations(self):
+        print(f"Matricule:{self.matricule}")
+        print(f"Marque: {self.marque}")
+        print(f"Couleur: {self.couleur}")
